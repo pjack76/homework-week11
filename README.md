5435e7e8955854c034bb7f1907bf43e89e892e62
Jack Long <putheara.long@gmail.com>
2020-04-22 22:34:24 -0400

    it's not working....

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # homework-week11
+
+note take app
+
+frontend codes provided, i'm supposed to write the backend then connect it to the frontend.  i got it read and write to the db.json, but some how html won't connect with index.js file.  i can't figure that out now.  i will ask questions later.
--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
+
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res){
+    let db = JSON.parse(fs.readFileSync("db/db.json"));
+    return res.json(db);
+});
+
+app.post("/api/notes", function(req, res){
+    let db = req.body;
+    console.log(req.body);
+    fs.writeFileSync("db/db.json", JSON.stringify(db));
+    res.json(db);
+});
+
 
 app.listen(port, function(){
     console.log(`Application server is listening on port: ${port}`);
